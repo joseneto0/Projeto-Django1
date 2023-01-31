aa589be38e0f74d8a36fe069847b08aa56fdac4e
José Neto <joseneto.jn1606@gmail.com>
2023-01-31 14:01:26 -0300

    Small changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'José Neto',
-        })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
+    })
